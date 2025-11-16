3d52d3f6b034ab44661fc2aaf69d2da72c17c3d5
Matthew Salmon <miniminnowcodes@gmail.com>
2025-11-16 09:10:49 +0000

    Win screens

---
+++ Assets/Scripts/Managers/DeathScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Globalization;
+using DG.Tweening;
+using Managers;
+using TMPro;
+using UnityEngine;
+
+namespace Managers
+{
+    public class DeathScreen : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI youDiedText;
+        [SerializeField] private Rigidbody2D playerRb;
+        [SerializeField] private Vector2 forceToAdd;
+        
+        private IEnumerator Start()
+        {
+            // Make "you scored" rise up from its OG position minus 5 on the Y axis, and
+            Vector2 ogPos = youDiedText.rectTransform.anchoredPosition;
+            Color ogCol = youDiedText.color;
+    
+            ogCol.a = 0;
+            youDiedText.color = ogCol;
+    
+            yield return new WaitForSeconds(1);
+
+            playerRb.bodyType = RigidbodyType2D.Dynamic;
+            playerRb.AddForce(forceToAdd, ForceMode2D.Impulse);
+            
+            youDiedText.rectTransform.anchoredPosition += Vector2.down;
+            youDiedText.rectTransform.DOAnchorPosY(ogPos.y, 0.5f);
+    
+            youDiedText.DOFade(1, 0.5f);
+            
+            yield return new WaitForSeconds(1f);
+            
+            SceneChangeManager.SwitchScene("MainLevel");
+        }
+    }
+}
--- Assets/WinScreenManager.cs
+++ Assets/WinScreenManager.cs
@@ -1,11 +1,69 @@
 using System.Collections;
+using System.Globalization;
+using DG.Tweening;
+using Managers;
+using TMPro;
 using UnityEngine;
 
 public class WinScreenManager : MonoBehaviour
 {
-    // [SerializeField] private 
+    [SerializeField] private TextMeshProUGUI youScoredText;
+    [SerializeField] private TextMeshProUGUI actualScoreText;
+    [SerializeField] private TextMeshProUGUI highscoreText;
+    [SerializeField] private GameObject playAgainButton;
+    
+    public static float actualScore = 10.48f;
+    
     private IEnumerator Start()
     {
-        yield break;
+        playAgainButton.SetActive(false);
+        actualScoreText.text = "";
+        // Make "you scored" rise up from its OG position minus 5 on the Y axis, and
+        Vector2 ogPos = youScoredText.rectTransform.anchoredPosition;
+        Color ogCol = youScoredText.color;
+        Color ogCol2 = highscoreText.color;
+
+        ogCol.a = 0;
+        ogCol2.a = 0;
+        youScoredText.color = ogCol;
+        highscoreText.color = ogCol2;
+
+        yield return new WaitForSeconds(2);
+        
+        youScoredText.rectTransform.anchoredPosition += Vector2.down;
+        youScoredText.rectTransform.DOAnchorPosY(ogPos.y, 0.5f);
+
+        youScoredText.DOFade(1, 0.5f);
+        
+        yield return new WaitForSeconds(0.75f);
+        
+        const float duration = 1;
+
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            float score = Mathf.Lerp(0, actualScore, t / duration);
+            actualScoreText.text = FormatTime(score);
+            yield return null;
+        }
+
+        actualScoreText.text = FormatTime(actualScore);
+
+        yield return new WaitForSeconds(0.5f);
+
+        highscoreText.text = "highscore: " + PlayerPrefs.GetFloat("highscore").ToString(CultureInfo.InvariantCulture);
+        Vector2 ogPos2 = highscoreText.rectTransform.anchoredPosition;
+        highscoreText.rectTransform.anchoredPosition += Vector2.down;
+        highscoreText.rectTransform.DOAnchorPosY(ogPos2.y, 0.5f);
+        highscoreText.DOFade(1, 0.5f);
+
+        yield return new WaitForSeconds(1f);
+        playAgainButton.SetActive(true);
+    }
+    
+    private static string FormatTime(float t) => $"{(int)t:00}:{(int)((t % 1) * 100):00}";
+
+    public void PlayAgain()
+    {
+        SceneChangeManager.SwitchScene("MainLevel");
     }
 }
