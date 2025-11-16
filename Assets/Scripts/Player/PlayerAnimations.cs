6cfc857914d014294bfb638ad6a0e79f35accf76
Matthew Salmon <miniminnowcodes@gmail.com>
2025-11-16 10:11:48 +0000

    stuff

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -4,11 +4,18 @@ namespace Player
 {
     public class PlayerAnimations : MonoBehaviour
     {
-        //[SerializeField] private AnimatiorController controller;
+        [SerializeField] private Animator controller;
 
         private void Update()
         {
-           // controller.
+            controller.SetFloat("vel", Mathf.Abs(Player.Input.Horizontal));
+
+            Player.Transform.localScale = Player.Rigidbody.linearVelocity.x switch
+            {
+                > 0.1f => new Vector3(-1, 1, 1),
+                < -0.1f => new Vector3(1, 1, 1),
+                _ => Player.Transform.localScale
+            };
         }
     }
 }
