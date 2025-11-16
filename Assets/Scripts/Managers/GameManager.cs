13695e27ee52d426e74c3dbea9da11efc35068bc
Matthew Salmon <miniminnowcodes@gmail.com>
2025-11-16 00:49:57 +0000

    message

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -38,15 +38,15 @@ namespace Managers
             timeSpent = 0;
         
             float hs = PlayerPrefs.GetFloat("highscore", -1);
-            highscoreText.text = Mathf.Approximately(hs, -1) ? "Highscore: None" : $"Highscore: {FormatTime(hs)}";
+            highscoreText.text = Mathf.Approximately(hs, -1) ? "Highscore: None" : $"Highscore: {FormatTime(hs)}<size=50%>s</size>";
         }
 
         private void UpdateTimerText()
         {
-            timerText.text = $"{FormatTime(timeSpent)}s";
+            timerText.text = $"{FormatTime(timeSpent)}<size=50%>s</size>";
         }
     
         // Format from float seconds to seconds:tenthOfASecond
-        private static string FormatTime(float t) => $"{(int)(t / 60):00}:{(int)(t % 60):00}.{(int)((t - (int)t) * 10)}";
+        private static string FormatTime(float t) => $"{(int)(t % 60):00}:{(int)(t - (int)t * 100):00}";
     }
 }
