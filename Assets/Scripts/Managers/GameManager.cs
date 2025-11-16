72ee594b43b6a8def6e6c63d0daf1249e7b13ca3
Matthew Salmon <miniminnowcodes@gmail.com>
2025-11-16 00:12:40 +0000

    world stuff

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using Managers;
+﻿using System;
+using Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,10 +8,30 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI highscoreText;
+
+    private float timeSpent;
     private void Awake() => Instance = this;
 
     public void RestartLevel()
     {
         SceneChangeManager.SwitchScene("MainLevel");
+    }
+
+    public void WinLevel()
+    {
+        
+    }
+
+    private void Update()
+    {
+        timeSpent += Time.deltaTime;
+    }
+
+    private void Start()
+    {
+        timeSpent = 0;
+        timerText.text
     }
 }
